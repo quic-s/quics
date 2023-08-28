da93fff42c5b44093e7d7ef0fa20587f03ec00af
seonwoo <ska2870ghk@naver.com>
2023-08-29 03:59:30 +0900

    feat: Add configuration for HTTP/3

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"crypto/tls"
 	"fmt"
+	"github.com/quic-go/quic-go"
+	"github.com/quic-s/quics/pkg/utils/env"
+	"github.com/quic-s/quics/pkg/utils/security"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/dgraph-io/badger/v3"
 	"github.com/gorilla/mux"
@@ -48,13 +53,27 @@ func connectHandler(db *badger.DB) *mux.Router {
 }
 
 func startHttp3Server(r *mux.Router) {
+	quicConfig := quic.Config{}
+
 	server := &http3.Server{
-		Addr:    ":8080",
-		Handler: r,
+		Addr:       ":8080",
+		QuicConfig: &quicConfig,
+		Handler:    r,
+	}
+
+	// get directory path for certification
+	quicsDir := env.GetDirPath()
+	certFileDir := filepath.Join(quicsDir, env.GetViperEnvVariables("QUICS_CERT_NAME"))
+	keyFileDir := filepath.Join(quicsDir, env.GetViperEnvVariables("QUICS_KEY_NAME"))
+
+	// load the certificate and the key from the files
+	_, err := tls.LoadX509KeyPair(certFileDir, keyFileDir)
+	if err != nil {
+		security.SecurityFiles()
 	}
 
 	fmt.Println("Starting HTTP/3 server...")
 
 	// TODO: need to link cretification files
-	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+	log.Fatal(server.ListenAndServeTLS(certFileDir, keyFileDir))
 }
