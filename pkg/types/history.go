f7d9ab4747b8cf0d6e1fbb7590e7de75c09cc84b
seonwoo <ska2870ghk@naver.com>
2023-10-02 12:13:50 +0900

    refactor: Reorganize all types used at internal and external

--- pkg/types/history.go
+++
@@ -1,9 +0,0 @@
-package types
-
-type FileHistory struct {
-	Id   uint64
-	Date string
-	UUID string
-	Path string       // path of stored the file with history
-	File FileMetadata // must have file metadata at the point that client wanted in time
-}
